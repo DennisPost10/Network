ff298f2bfe554e0fc3f33f70eb5d7b67d68b79b1
postd <postd@boole.bio.ifi.lmu.de>
2017-07-17 20:54:30 +0200

    update

--- utils/MultiFastaParser.py
+++ utils/MultiFastaParser.py
@@ -1,20 +1,46 @@
 import sys
-
+import re
 import requests
 
+regex = "class=\"reportsequence\" NOWRAP width=\"85%\".*\n"
+pattern = re.compile(regex)
 
-def get_source_code(prot_name):
+def get_q3_state(q8_state):
+        
+    if q8_state == 'H':
+        return 'H'
+    elif q8_state == 'G':
+        return 'H'
+    elif q8_state == 'E':
+        return 'E'
+    elif q8_state == 'B':
+        return 'E'
+    else:
+        return 'C'
+
+def get_ss_from_source_code(prot_name):
     prot = prot_name[:-1]
     chainId = prot_name[-1:]
     url = 'https://www.rcsb.org/pdb/explore/sequenceText.do?structureId=' + prot + '&chainId=' + chainId
 
-    response = requests.get(url)
-    print(response.content)
+    response = requests.get(url).text
+    matches = re.findall(pattern, response)
+    ss = ""
+    for match in matches:
+        ss+=match.strip().split(">")[1].replace("&nbsp;", "-")
 
+    return ss
 
+def parse_q3(ss):
+    ss_q3 = ""
+    for x in ss:
+        ss_q3 += get_q3_state(x)
+    return ss_q3
+        
 def read_fasta(mult_fasta_file, filter_set):    
     header = None
     seq = ""
+    x=0
     with open(mult_fasta_file) as f:
         line = f.readline().strip()
         while line:
@@ -22,7 +48,7 @@ def read_fasta(mult_fasta_file, filter_set):
                 if header == None:
                     header = line
                 else:
-                    add_chain(header, seq, filter_set)
+                    x+=add_chain(header, seq, filter_set)
                     header = line
                     seq = ""
             else:
@@ -31,17 +57,33 @@ def read_fasta(mult_fasta_file, filter_set):
             line = f.readline().strip()
         
         if header != None:
-            add_chain(header, seq, filter_set)
-            
+            x+=add_chain(header, seq, filter_set)
+    print(x)    
+        
 def add_chain(header, sequence, filter_set):
+    state = 0
     name, equis = get_parsed_header(header)
-    if name in filter_set or any(x in filter_set for x in equis):
+    ss = None
+    if name in filter_set:# or any(x in filter_set for x in equis):
         sequence = get_parsed_seq(sequence)
-        get_source_code(name) 
-        #print(name)
-        #print(equis)
-        #print(sequence)
-    
+        ss = get_ss_from_source_code(name)
+        ss_q3 = parse_q3(ss)
+        print(name)
+        state = 1
+    else:
+        for x in equis:
+            if x in filter_set:
+                ss = get_ss_from_source_code(x)
+                ss_q3 = parse_q3(ss)
+                print(name)
+                print(x)                
+                state = 1
+    if ss != None:
+        print(equis)
+        print(ss)
+        print(ss_q3)
+    return state
+
 def get_parsed_seq(sequence):
     
     return sequence
